ad599100882e9aebf728ad9b182db360703c27f0
Ian Turgeon <iturgeon@mail.ucf.edu>
2010-12-17 18:10:56 -0500

    issues with setting course link failure

--- internal/plugin/UCFCourses/classes/UCFCoursesAPI.class.php
+++ internal/plugin/UCFCourses/classes/UCFCoursesAPI.class.php
@@ -271,7 +271,7 @@ class plg_UCFCourses_UCFCoursesAPI extends core_plugin_PluginAPI
 						".cfg_plugin_UCFCourses::MAP_COL_ID." = '',
 						".cfg_plugin_UCFCourses::MAP_COL_NAME." = '',";
 			
-			return $this->DBM->querySafe($sql, $instID, $sectionID, $userID, $sectionID, $userID);
+			return (bool) $this->DBM->querySafe($sql, $instID, $sectionID, $userID, $sectionID, $userID);
 		}
 		else
 		{
