b6642f642e3c2d93f8bd8daad85ce9da14290eba
Ian Turgeon <iturgeon@mail.ucf.edu>
2010-12-17 17:52:09 -0500

    setCourseForInstance returns either true or an error object now

--- internal/plugin/UCFCourses/classes/UCFCoursesAPI.class.php
+++ internal/plugin/UCFCourses/classes/UCFCoursesAPI.class.php
@@ -270,9 +270,8 @@ class plg_UCFCourses_UCFCoursesAPI extends core_plugin_PluginAPI
 						". cfg_core_User::ID." = '?',
 						".cfg_plugin_UCFCourses::MAP_COL_ID." = '',
 						".cfg_plugin_UCFCourses::MAP_COL_NAME." = '',";
-			$this->DBM->querySafe($sql, $instID, $sectionID, $userID, $sectionID, $userID);
 			
-			return $result;
+			return $this->DBM->querySafe($sql, $instID, $sectionID, $userID, $sectionID, $userID);
 		}
 		else
 		{
