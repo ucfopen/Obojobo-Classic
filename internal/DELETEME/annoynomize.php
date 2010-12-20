ba13c88bfb94aa14375c6615acde06747d1fa201
Ian Turgeon <iturgeon@gmail.com>
2010-12-20 13:59:32 -0500

    made the annoynomizer more genaric

--- internal/DELETEME/annoynomize.php
+++ internal/DELETEME/annoynomize.php
@@ -1,5 +1,5 @@
 <?php
-require_once(dirname(__FILE__)."/../app.php");
+require_once(dirname(__FILE__) . '/../app.php');
 
 $DBM = core_db_DBManager::getConnection(new core_db_dbConnectData(AppCfg::DB_HOST, AppCfg::DB_USER, AppCfg::DB_PASS, AppCfg::DB_NAME, AppCfg::DB_TYPE));
 
@@ -13,19 +13,18 @@ while( $r = $DBM->fetch_obj($q))
 	$index = rand(0, count($firsts)-1 );
 	$email = $firsts[$index] . "@dudes.ucf.edu";
 	
-	$q2 = $DBM->query("UPDATE ".cfg_core_User::TABLE." SET first = '".$firsts[$index]."', last = '".$lasts[$index]."', email = '$email' WHERE userID = '". $r->userID."' AND userID != '1' AND userID != '3'");
+	$q2 = $DBM->query("UPDATE ".cfg_core_User::TABLE." SET ".cfg_core_User::FIRST." = '".$firsts[$index]."', ".cfg_core_User::LAST." = '".$lasts[$index]."', ".cfg_core_User::EMAIL." = '$email' WHERE ".cfg_core_User::ID." = '". $r->userID."' AND ".cfg_core_User::ID." != '1' AND ".cfg_core_User::ID." != '3'");
 }
-
-
 
 
 $pass = md5(time());
 $salt = md5(microtime(1));
 
-$DBM->query("UPDATE ".cfg_plugin_AuthModUCF." SET login = CONCAT('Anonymous', userID), password = '$pass', salt = '$salt'");
+$DBM->query("UPDATE ".cfg_plugin_AuthModUCF::TABLE." SET ".cfg_plugin_AuthModUCF::USER_NAME." = CONCAT('Anonymous', ".cfg_core_User::ID."), ".cfg_plugin_AuthModUCF::PASS." = '$pass', ".cfg_plugin_AuthModUCF::SALT." = '$salt'");
 
+$DBM->query("UPDATE ".cfg_core_AuthModInternal::TABLE." SET ".cfg_core_AuthModInternal::USER_NAME." = CONCAT('~Anonymous', ".cfg_core_User::ID."), ".cfg_core_AuthModInternal::PASS." = '$pass', ".cfg_core_AuthModInternal::SALT." = '$salt' WHERE ".cfg_core_User::ID." != '3' AND ".cfg_core_User::ID." != '1'");
 
-$DBM->query("UPDATE ".cfg_core_AuthModInternal." SET login = CONCAT('~Anonymous', userID), password = '$pass', salt = '$salt' WHERE userID != '3' AND userID != '1'");
+// TODO: Empty MODX  web_users, web_groups and  web_users_attributes
 
 echo "done";
 
