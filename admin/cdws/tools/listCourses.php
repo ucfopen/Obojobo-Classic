ea4fd1ee87505dfd02ab508fd1b47c2891979909
Ian Turgeon <iturgeon@gmail.com>
2011-01-07 13:46:53 -0500

    bug fix in list courses

--- admin/cdws/tools/listCourses.php
+++ admin/cdws/tools/listCourses.php
@@ -30,6 +30,8 @@ if(! in_array('SuperUser', $result['hasRoles']) )
 
 if(!empty($_GET['nid']) )
 {
+	echo ""
+	$PM = core_plugin_PluginManager::getInstance();
 	$result = $PM->callAPI('UCFCourses', 'testOnlyGetCourses', array($_GET['nid']), true);
 	echo $_GET['nid'] . "'s Courses:<br>";
 	print_r($result);
