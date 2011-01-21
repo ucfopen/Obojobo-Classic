5553af7dc84b65cea3a6e3e9b5b9b977a19aff6b
Zachary Berry <zachberry@gmail.com>
2011-01-21 14:02:07 -0500

    added email checks to userInfo helper script

--- admin/cdws/userInfo.php
+++ admin/cdws/userInfo.php
@@ -13,6 +13,7 @@ $DBM = core_db_DBManager::getConnection(new core_db_dbConnectData(AppCfg::DB_HOS
 
 $nid = $_GET['nid'];
 $uid = 0;
+$email = '';
 $result = $DBM->query("SELECT userID FROM `obo_user_auth_ucf` WHERE login = '$nid'");
 $r = $DBM->fetch_assoc($result);
 $uid = $r['userID'];
@@ -32,12 +33,33 @@ if(is_numeric($uid) && $uid > 0)
 	
 	echo '<h2>User Info:</h2><hr>';
 	echo '<h3>'.$result->first.' '.$result->last.' - '.$nid.'</h3>';
+	$email = $result->email;
 	
 	writeResult('User is in los database', is_numeric($result->userID));
 	writeData($result);
 	writeHelp('If the user is in the database that means they attempted to login and put in a valid NID. It doesn\'t mean they logged in successfully.');
-	writeResult('User has email', isRealStr($result->email));
+	
+	writeResult('User has email', isRealStr($email));
 	writeHelp('User must have email.');
+	
+	if(isRealStr($email))
+	{
+		$q = 'SELECT * FROM obo_users WHERE email = "'.$email.'"';
+		$result = $DBM->query($q);
+		$results = array();
+		while($r = $DBM->fetch_assoc($result))
+		{
+			$results[] = $r;
+		}
+		writeResult('Another Obojobo user doesn\'t share the same email address', sizeof($results) == 1);
+		if(sizeof($results) != 1)
+		{
+			echo '<br>Here are the users with that email:';
+			writeData($results);
+		}
+		writeHelp('No user should have the same email.  If they do, this is a problem that needs to be resolved by New Media.');
+	}
+	
 	writeResult('User has lastLogin', is_numeric($lastLogin) && $lastLogin > 0);
 	if(is_numeric($lastLogin) && $lastLogin > 0)
 	{
@@ -82,10 +104,36 @@ if(is_numeric($uid) && $uid > 0)
 	writeData($r);
 	writeData($modxAttrib);
 	writeHelp('User most likely should be in the ModX database.  If not, then either ModX had a problem and couldn\'t create them or ModX was not involved for some reason.');
+	
 	writeResult('User has e-mail in modx', isRealStr($modxAttrib['email']));
 	writeHelp('This is not required.');
+	
+	writeResult('E-mail in Obojobo and ModX match', $modxAttrib['email'] == $email);
+	writeHelp('These should match although it is not required.');
+	
+	if(isRealStr($modxAttrib['email']))
+	{
+		$results = array();
+		$q = 'SELECT * FROM modx_web_user_attributes WHERE email = "'.$modxAttrib['email'].'"';
+		$result = $DBM->query($q);
+		while($r = $DBM->fetch_assoc($result))
+		{
+			$results[] = $r;
+		}
+		
+		writeResult('Another ModX user doesn\'t share the same email address', sizeof($results) == 1);
+		if(sizeof($results) != 1)
+		{
+			echo '<br>Here are all the users with this email address:';
+			writeData($results);
+		}
+		writeHelp('No other user in ModX should be sharing an email address with this user.  If so, there is a problem that must be resolved by New Media.');
+	}
+	
+	
 	writeResult('User is not blocked in modx', $modxAttrib['blocked'] == 0);
 	writeHelp('If the user is blocked that means they incorrectly logged in a significant number of times.  They won\'t be able to use the system until they are unblocked by New Media.');
+	
 	echo '<br> - <b>ModX Failed login count:</b> '.$modxAttrib['failedlogincount'];
 	writeHelp('This number represents the number of times a user failed logging into the website login (not the Repository or Viewer login, which is separate).  If this number is high it may mean that there is a problem with the website login.  You may consider asking the user to login directly in the Repository or Viewer.');
 	
