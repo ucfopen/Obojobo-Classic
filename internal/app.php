ebfc2230c219a5b7942307796b4f1a6d8ecce9e4
Ian Turgeon <iturgeon@gmail.com>
2011-02-25 15:28:39 -0500

    added fixes to the log data

--- internal/app.php
+++ internal/app.php
@@ -26,50 +26,41 @@ function trace($arg, $force=0, $incbacklog=0)
 function classAutoLoader($className)
 {
 	
-	// convert namespace to full file path  
-//    $class = 'classes/' . str_replace('\\', '/', $class) . '.php';  
-//    require_once($class);
 
-	// look at the first 4 characters to determine if its a plugin, config, or regular class file
+	// classname is using namespaces
 	if(strpos($className , '\\') !== false)
 	{
+		
 		$file = \AppCfg::DIR_BASE . \AppCfg::DIR_CLASSES  . str_replace('\\', '/', $className) . '.class.php';
-		if(@include($file))
-		{
-			return;
-		}
-		else
-		{
-			error_log('loading file with namespace ' . $className);
-		}
 	}
-
-	$prefix = substr($className, 0, 4);
-	switch($prefix)
+	// classname is using old nm_class package notation
+	else
 	{
-		case 'plg_': // look in the plugin dir EX: plugin_UCFCourses_UCFCoursesAPI.class.php
-			// convert plugins_pluginName_ClassName to plugins/pluginName/classes/ClassName.class.php
-			$pkgs = explode("_", $className);
-			$file = \AppCfg::DIR_BASE . \AppCfg::DIR_PLUGIN.$pkgs[1].'/classes/';
-			unset($pkgs[0]);
-			unset($pkgs[1]);
-			$file = $file . implode('/', $pkgs) . '.class.php';
-			break;
-		case 'cfg_': // look in the config dir EX: config_plugin_UCFCourses
-			$file = substr($className, 4);
-			$file = CONFIG_ROOT . '/' .str_replace('_', '/', $file) . '.class.php';
-			break;
-		default: // Look in the app classes dir EX: \rocketD\auth\AuthModule
-			error_log($className);
-			$file = \AppCfg::DIR_BASE . \AppCfg::DIR_CLASSES . str_replace('_', '/', $className) . '.class.php';
+		$prefix = substr($className, 0, 4);
+		switch($prefix)
+		{
+			case 'plg_': // look in the plugin dir EX: plugin_UCFCourses_UCFCoursesAPI.class.php
+				// convert plugins_pluginName_ClassName to plugins/pluginName/classes/ClassName.class.php
+				$pkgs = explode("_", $className);
+				$file = \AppCfg::DIR_BASE . \AppCfg::DIR_PLUGIN.$pkgs[1].'/classes/';
+				unset($pkgs[0]);
+				unset($pkgs[1]);
+				$file = $file . implode('/', $pkgs) . '.class.php';
+				break;
+			case 'cfg_': // look in the config dir EX: config_plugin_UCFCourses
+				$file = substr($className, 4);
+				$file = CONFIG_ROOT . '/' .str_replace('_', '/', $file) . '.class.php';
+				break;
+			default: // Look in the app classes dir EX: \rocketD\auth\AuthModule
+				error_log($className);
+				$file = \AppCfg::DIR_BASE . \AppCfg::DIR_CLASSES . str_replace('_', '/', $className) . '.class.php';
+		}
 	}
-	
 	// try to include
 	if(!@include($file))
 	{
 		// log error on failure
 		@$dt = debug_backtrace();
-		error_log($file);
 		error_log('autoload failed to load class "'. basename($className) . '" referenced from ' . basename($dt[2]['file']) . '#' . $dt[2]['line'] . ' -> ' . $dt[2]['function']);
 	}
 }
