e29447a9641b97f38666f851b8d0483917b1fe63
Ian Turgeon <iturgeon@gmail.com>
2011-05-27 18:13:37 -0400

    started page switching

--- wp/wp-content/themes/obojobo/page-html-viewer.php
+++ wp/wp-content/themes/obojobo/page-html-viewer.php
@@ -4,21 +4,9 @@ Template Name: HTML Viewe
 */
 
 get_header('html-viewer'); ?>
-<div id="section-overview" class="section section-active">
-	<h1><span id="title">title</span> <span id="version">version</span></h1>
-	Learn Time: <span id="learn-time">learn time</span> minutes.
-	<h2>Objective:</h2>
-	<span id="objective"></span>
+<div id="navigation"><a href="#Overview">Ovierview</a> <a href="#Content">Content</a> <a href="#Practice">Practice</a> <a href="#Assessment">Assessment</a></div>
 
-	<h2>Keywords</h2>
-	<span id="key-words">key words</span></p>
-	
-	<h2>Pages:</h2>
-	<p>Content Pages: <span id="content-size">content-size</span></p>
-	<p>Practice Questions: <span id="practice-size">practice-size</span></p>
-	<p>Assessment Questions: <span id="assessment-size">assessment-size</span></p>
-	<a href="#Content">Load Content</a>
-</div>
+<div id="section-overview" class="section"></div>
 
 <div id="section-content" class="section"></div>
 
@@ -26,6 +14,6 @@ get_header('html-viewer'); ?>
 
 <div id="section-assessment" class="section"></div>
 
-<div id="section-feedback" class="section"></div>
+<div id="section-review" class="section"></div>
 
 <?php get_footer('html-viewer'); ?>
