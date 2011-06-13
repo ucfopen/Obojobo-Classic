a37bd377684c2c2b8b781c98f32b6e7114c624ed
Ian Turgeon <iturgeon@gmail.com>
2011-06-12 22:54:02 -0400

    cleaning up navigation html and js

--- wp/wp-content/themes/obojobo/page-html-viewer.php
+++ wp/wp-content/themes/obojobo/page-html-viewer.php
@@ -4,18 +4,9 @@ Template Name: HTML Viewe
 */
 
 get_header('html-viewer'); ?>
-<div id="navigation"><a id="nav-overview" href="#">Ovierview</a> <a id="nav-content" href="#">Content</a> <a id="nav-practice" href="#">Practice</a> <a id="nav-assessment" href="#">Assessment</a></div>
 
-<div id="sub-navigation"></div>
+<div id="navigation"></div>
 
-<div id="section-overview" class="section"></div>
-
-<div id="section-content" class="section"></div>
-
-<div id="section-practice" class="section"></div>
-
-<div id="section-assessment" class="section"></div>
-
-<div id="section-review" class="section"></div>
+<div id="content"></div>
 
 <?php get_footer('html-viewer'); ?>
