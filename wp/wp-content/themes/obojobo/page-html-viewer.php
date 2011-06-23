691eccd12a6024b4b255dbbfda857220becbef13
Ian Turgeon <iturgeon@gmail.com>
2011-06-22 23:58:58 -0400

    Overview pages and finishing next button action
    Added Content Transition page
    Added Practice Section Overview
    Added Assessment Section Overview

--- wp/wp-content/themes/obojobo/page-html-viewer.php
+++ wp/wp-content/themes/obojobo/page-html-viewer.php
@@ -16,12 +16,12 @@ add_filter('show_admin_bar', '__return_false');
 <!-- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script> -->
 <script type="text/javascript" src="/assets/js/swfobject.js"></script>
 <script type="text/javascript" src="/assets/js/viewer.js"></script>
-<script type="text/javascript" src="/assets/js/jquery.history.js"></script>
+<!-- <script type="text/javascript" src="/assets/js/jquery.history.js"></script> -->
 
 
 <link rel="stylesheet" type="text/css" href="/assets/css/viewer-screen.css" media="screen" />
 <!-- <link rel="stylesheet" type="text/css" href="/assets/css/tablet.css" media="screen and (max-width: 957px)" /> -->
-<link rel="stylesheet" type="text/css" href="/assets/css/viewer-phone.css" media="screen and (max-device-width: 500px) and (orientation: portrait)" />
+<!-- <link rel="stylesheet" type="text/css" href="/assets/css/viewer-phone.css" media="screen and (max-device-width: 500px) and (orientation: portrait)" /> -->
 <link rel="stylesheet" type="text/css" href="/assets/css/viewer.css"> 
 
 
@@ -31,17 +31,5 @@ var loID = <?php echo $wp_query->query_vars['loID']; ?>;
 </script>
 </head>
 <body <?php body_class(); ?>>
-
-
-<header id='header'>
-	<h1 id="lo-title"></h1>
-	<div id='logo'>powered by Obojobo</div>
-	<nav id="navigation"></nav>
-</header>
-
-<div id="content"></div>
-
-
-<div id="footer">footer</div>
 
 <?php get_footer('html-viewer'); ?>
