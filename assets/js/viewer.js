efb56413347b17a135b2f2148aa7b4e299e7c956
Zachary Berry <zachberry@gmail.com>
2011-07-20 12:36:43 -0400

    adding answer feedback

--- assets/js/viewer.js
+++ assets/js/viewer.js
@@ -494,7 +494,7 @@ function onAnswerRadioClicked(event)
 			}
 			if(viewerMode == MODE_PREVIEW)
 			{
-				$(event.target).parent().append('<span class="answer-preview '+ weightCSS +'">'+answer.weight+'%</span>');
+				$(event.target).parent().append('<span class="answer-preview '+ weightCSS +'">'+answer.weight+'%</span><span class="answer-feedback"><h4>Review:</h4><p>lol</p></span>');
 			}
 			return true;
 		}
