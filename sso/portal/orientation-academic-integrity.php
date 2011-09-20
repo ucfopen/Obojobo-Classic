e0e49fd42b399dfb2d23e66285ae9b93a36f56b5
Ian Turgeon <iturgeon@gmail.com>
2011-09-20 12:58:03 -0400

    SSO Portal Page update for Spring 2011
    Portal pagelet updated to display 4 links in two categories.
    One for Transfer students, one for freshmen.
    Also updated the query to remove it's reference to the LO table,
    it wasn't needed.

--- sso/portal/orientation-academic-integrity.php
+++ sso/portal/orientation-academic-integrity.php
@@ -17,16 +17,13 @@ try
 	// Testing for ian's user
 	// if($_SERVER['HTTP_REMOTE_USER'] == 'i0396856')
 	// {
-		
-		
 	// }
 
 	$isDevEnvironment = preg_match('/^https:\/\/(patest|padev)\d*?\.net\.ucf\.edu\/psp\/PA(TEST|DEV)/', $_SERVER['HTTP_REFERER']);
-
 	// show a default set of learning objects in a dev environment
 	if($isDevEnvironment)
 	{
-		$los = array('2329','2330');
+		$los = array('2329','2329','2330','2330');
 	}
 	else
 	{
@@ -39,6 +36,7 @@ try
 	// $timestamp = time();
 	// $hash = md5($NID.$timestamp.\AppCfg::UCF_PORTAL_SECRET);
 	// $los = explode(',', \AppCfg::UCF_PORTAL_ORIENTATION_INSTANCES);
+	
 	
 	// valid hash
 	if(md5($NID.$timestamp.\AppCfg::UCF_PORTAL_SECRET) === $hash && (int)$timestamp >= time() - \AppCfg::UCF_PORTAL_TIMEOUT /*30 minutes ago*/)
@@ -55,10 +53,8 @@ try
 		$DBM = \rocketD\db\DBManager::getConnection(new \rocketD\db\DBConnectData(\AppCfg::DB_HOST, \AppCfg::DB_USER, \AppCfg::DB_PASS, \AppCfg::DB_NAME, \AppCfg::DB_TYPE));
 		$qstr = "SELECT I.instID, MAX( A.score ) AS score, I.name
 			FROM obo_lo_instances AS I
-			JOIN obo_los AS L
-			ON L.loID = I.loID
 			LEFT JOIN obo_log_attempts AS A
-			ON A.instID = I.instID AND L.aGroupID = A.qGroupID AND A.userID =  '?'
+			ON A.instID = I.instID AND A.endTime > 0 AND A.userID =  '?'
 			WHERE I.instID
 			IN ( ? ) 
 			GROUP BY I.instID";
@@ -88,6 +84,33 @@ try
 		exit();
 	}
 	
+	function formatDisplayForInstance($instID, $instName, $score, $minScore, $targetURL, $hash)
+	{
+		$output = '';
+		if($instID && $instName)
+		{
+			// No Score
+			if($score === NULL)
+			{
+				$output = '<p style="font-size: 12pt; margin-bottom: 0;"><a style="font-weight:bold;" href="'.$targetURL.'?instID='.$instID.$hash.'" target="_blank" proxied="false">'.$instName.'</a></p>';
+				$output .= '<p style="margin-top: 0; font-size: 8pt; color: #990000;">Not yet complete</p>';
+			}
+			// Completed & Score is above min
+			else if( (int)$score >= (int)$minScore)
+			{
+				$output = '<p style="font-size: 12pt; margin-bottom: 0;">'.$instName.'</p>';
+				$output .= '<p style="margin-top: 0; font-size: 8pt;"><span style="color: green">Completed</span> with a score of '.$score.'% (<a  href="'.$targetURL.'?instID='.$instID.$hash.'" target="_blank" proxied="false">Take again</a>)</p>';
+			}
+			// Completed BUT score is below min
+			else
+			{
+				$output = '<p style="font-size: 12pt; margin-bottom: 0;"><a style="font-weight:bold;" href="'.$targetURL.'?instID='.$instID.$hash.'" target="_blank" proxied="false">'.$instName.'</a></p>';
+				$output .= '<p style="margin-top: 0; font-size: 8pt;"><span style="color: #990000">Not yet complete.</span> Your score of <strong style="color: #990000">'.$score.'%</strong> is below the minimum of '.$minScore.'%.</p>';
+			}
+		}
+		return $output;
+	}
+	
 	// Display html
 	?>
 	<html>
@@ -98,39 +121,32 @@ try
 		if(count($scores) > 0)
 		{
 			?>
-
-			<p>Are you an incoming student?  If so, you're required to complete the Academic Integrity modules listed below.  Click on each of these module links which will open in a new window.  You'll need to score a <?php echo $minScore; ?>% or better on each module to pass.</p>
-			<ul>
+			<p>Are you an Incoming student? If so, you're required to complete the Academic Integrity Modules listed below. The links below are different for first time college students and transfer students.</p>
+			<p>Click on each of the links that apply to you and a new window will open with the Academic Integrity module in it. You will need to score <?php echo $minScore; ?>% or better on each module to pass.</p>
 			<?php
-
-				foreach($los AS $key => $instID)
-				{
-					if(isset($scores[$key]))
-					{
-					?>
-					<li>
-						
-						<?php if($scores[$key]->score){
-							if((int)($scores[$key]->score) >= (int)$minScore){?>
-								<p style="font-size: 12pt; margin-bottom: 0;"><?php echo $scores[$key]->name; ?></p>
-								<p style="margin-top: 0; font-size: 8pt;"><span style="color: green">Completed</span> with a score of <?php echo $scores[$key]->score; ?>% (<a  href="<?php echo $targetURL;?>?instID=<?php echo $scores[$key]->instID . $hashAppend; ?>" target="_blank" proxied="false">Take again</a>)</p>
-							<?php }else{?>
-								<p style="font-size: 12pt; margin-bottom: 0;"><a style="font-weight:bold;" href="<?php echo $targetURL;?>?instID=<?php echo $scores[$key]->instID . $hashAppend; ?>" target="_blank" proxied="false"><?php echo $scores[$key]->name; ?></a></p>
-								<p style="margin-top: 0; font-size: 8pt;"><span style="color: #990000">Not yet complete.</span> Your score of <strong style="color: #990000"><?php echo $scores[$key]->score; ?>%</strong> is below the minimum of <?php echo $minScore; ?>%.</p>
-							<?php }
-						} 
-						else { ?>
-							<p style="font-size: 12pt; margin-bottom: 0;"><a style="font-weight:bold;" href="<?php echo $targetURL;?>?instID=<?php echo $scores[$key]->instID . $hashAppend; ?>" target="_blank" proxied="false"><?php echo $scores[$key]->name; ?></a></p>
-							<p style="margin-top: 0; font-size: 8pt; color: #990000;">Not yet complete</p>
-						<?php } ?>
-					</li>
-					<?php
-					}
-				}
+				if($scores[0] || $scores[1])
+				{?>
+					<h3>Transfer Students</h3>
+						<ul>
+						<?php
+							echo formatDisplayForInstance($los[0], $scores[0]->name, $scores[0]->score, $minScore, $targetURL, $hashAppend);
+							echo formatDisplayForInstance($los[1], $scores[1]->name, $scores[1]->score, $minScore, $targetURL, $hashAppend);
+						?>
+						</ul>
+				<?}
+				if($scores[2] || $scores[3])
+				{?>
+					<h3>First-time College Students</h3>
+						<ul>
+						<?php
+							echo formatDisplayForInstance($los[2], $scores[2]->name, $scores[2]->score, $minScore, $targetURL, $hashAppend);
+							echo formatDisplayForInstance($los[3], $scores[3]->name, $scores[3]->score, $minScore, $targetURL, $hashAppend);
+						?>
+						</ul>
+				<?}
 				?>
-			</ul>
 
-			<p>You will need to complete these before <strong>August 22nd 2011</strong>.  Otherwise you will receive a hold on your account that will prevent you from registering for classes.</p>
+			<p>You will need to complete these before <strong>January 9nd 2012</strong>.  Otherwise you will receive a hold on your account that will prevent you from registering for classes.</p>
 			<?php
 			if($isDevEnvironment)
 			{
