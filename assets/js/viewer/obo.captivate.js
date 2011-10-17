d4d4a29d42b286fd7951b748a5602bfc7b15339d
Zachary Berry <zachberry@gmail.com>
2011-10-17 13:30:28 -0400

    first commit of zachs changes to htmo html5 viewer - backend integration, captivate 2/5 support, blue theme, revealing module pattern for js, working jwplayer integration, layout design tweaks, and more!

---
+++ assets/js/viewer/obo.captivate.js
@@ -0,0 +1,81 @@
+// Handles scoring from captivate spy swfs and 
+
+if(!window.obo)
+{
+	window.obo = {};
+}
+
+obo.captivate = function()
+{
+	// we store which id we are recording captivate data for. If this changes we need to clear out
+	// our score data and start over.
+	var captivateID = -1;
+	// we store the captivate score data for captivate 5 swfs since we are responsible for
+	// calculating the grade
+	var scoreData;
+	
+	// public method which ExternalInterface calls whenever it gets a captivate event.
+	// look in the CaptivateSpy fla files for the ExternalInterface code
+	var onCaptivateSpyEvent = function(event)
+	{
+		console.log('onCaptivateSpyEvent');
+		console.log(event, event.type);
+		
+		// if this event is for a different id then reset our data
+		if(event.id != captivateID)
+		{
+			captivateID = event.id;
+			scoreData = {responses:[], numQuestionsAnswered:0};
+		}
+		
+		// we listen for two versions of captivate - 2 (AS2) and 5 (AS3):
+		switch(event.version)
+		{
+			case 2:
+				switch(event.type)
+				{
+					case 'score':
+						obo.view.updateInteractiveScore(Math.round(event.data.percent * 100));
+						break;
+				}
+				break;
+			case 5:
+				switch(event.type)
+				{
+					case 'CPInteractiveItemSubmitEvent':
+					case 'CPQuestionSubmitEvent':
+						var page = obo.model.getPage();
+						
+						var questionEventData = event.data.questionEventData;
+						
+						var index = questionEventData.questionNumber;
+						//check to see if the question was already answered
+						if(scoreData.responses[index] == undefined)
+						{
+							scoreData.numQuestionsAnswered++;
+						}
+						
+						// store response
+						scoreData.responses[index] = (questionEventData.questionScore / questionEventData.questionMaxScore);
+						
+						// if all questions answered, send info to obojobo
+						var total = 0;
+						for(var i in scoreData.responses)
+						{
+							total += scoreData.responses[i];
+						}
+						
+						var percent = Math.round((total / event.data.cpQuizInfoTotalQuestionsPerProject) * 100);
+						
+						obo.view.updateInteractiveScore(percent);
+						break;
+				}
+				break;
+		}
+	};
+	
+	// @public:
+	return {
+		onCaptivateSpyEvent: onCaptivateSpyEvent
+	}
+}();
--- wp/wp-content/themes/obojobo/page-html-viewer.php
+++ wp/wp-content/themes/obojobo/page-html-viewer.php
@@ -1,33 +1,93 @@
 <?php
 /*
-Template Name: HTML Viewe
+Template Name: HTML Viewer
 */
 add_filter('show_admin_bar', '__return_false');
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<!--[if IE]><script>/*@cc_on'abbr article aside audio canvas details figcaption figure footer header hgroup mark menu meter nav output progress section summary time video'.replace(/\w+/g,function(n){document.createElement(n)})@*/</script><![endif]--> 
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <title></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!-- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script> -->
 <script type="text/javascript" src="/assets/js/jquery/1.6.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
 <!-- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script> -->
+<script type="text/javascript" src="/assets/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+<!--<script type="text/javascript" src="/assets/js/poshytip/src/jquery.poshytip.min.js"></script>-->
+<script type="text/javascript" src="/assets/js/tipTipv13/jquery.tipTip.js"></script>
+<script type="text/javascript" src="/assets/js/jquery.activity-indicator-1.0.0.min.js"></script>
+<script type="text/javascript" src="/assets/js/jquery.simplemodal.1.4.1.min.js"></script>
+<!--<script type="text/javascript" src="/assets/flowplayer/flowplayer-3.2.6.min.js"></script>-->
 <script type="text/javascript" src="/assets/js/swfobject.js"></script>
+<!--<script type="text/javascript" src="/assets/jwplayer/jwplayer.js"></script>-->
+
+<script type="text/javascript" src="/assets/js/viewer/obo.util.js"></script>
+<script type="text/javascript" src="/assets/js/viewer/obo.model.js"></script>
+<script type="text/javascript" src="/assets/js/viewer/obo.remote.js"></script>
+<script type="text/javascript" src="/assets/js/viewer/obo.view.js"></script>
+<script type="text/javascript" src="/assets/js/viewer/obo.dialog.js"></script>
+<script type="text/javascript" src="/assets/js/viewer/obo.loader.js"></script>
+<script type="text/javascript" src="/assets/js/viewer/obo.media.js"></script>
+<script type="text/javascript" src="/assets/js/viewer/obo.captivate.js"></script>
+
+<!--
 <script type="text/javascript" src="/assets/js/viewer.js"></script>
+-->
+
 <!-- <script type="text/javascript" src="/assets/js/jquery.history.js"></script> -->
 
+<link rel="stylesheet" type="text/css" href="/assets/js/poshytip/src/tip-twitter/tip-twitter.css">
+<link rel="stylesheet" type="text/css" href="/assets/js/tipTipv13/tipTip.css">
+<!--<link rel="stylesheet" type="text/css" href="/assets/flowplayer/style.css">-->
 
-<link rel="stylesheet" type="text/css" href="/assets/css/viewer-screen.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="/assets/css/themes/classic.css" media="screen">
+<link rel="stylesheet" type="text/css" href="/assets/css/ui-lightness/jquery-ui-1.8.16.custom.css" media="screen">
 <!-- <link rel="stylesheet" type="text/css" href="/assets/css/tablet.css" media="screen and (max-width: 957px)" /> -->
 <!-- <link rel="stylesheet" type="text/css" href="/assets/css/viewer-phone.css" media="screen and (max-device-width: 500px) and (orientation: portrait)" /> -->
-<link rel="stylesheet" type="text/css" href="/assets/css/viewer.css"> 
 
+
+<link id="theme-blue" rel="stylesheet" type="text/css" href="/assets/css/themes/blue.css" media="screen" />
+<link rel="stylesheet" href="/assets/js/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
+
+<link href='http://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
+
+
+<script type="text/javascript" src="/assets/js/modernizr-2.0.6.js"></script>
 
 <?php wp_head(); ?>
 <script type="text/javascript" defer="defer">	
-var loID = <?php echo $wp_query->query_vars['loID']; ?>; 
+	$(function() {
+		
+		//@TODO: are these the right params?
+		var params = {
+			loID:'<?php echo $wp_query->query_vars['loID']; ?>',
+			instID:'<?php echo $wp_query->query_vars['instID']; ?>'
+		};
+		/*
+		//@TODO - this syntax is weird
+		obo.model = obo.model(obo.view, {});
+		obo.model.loadLO(loID, function() {
+			obo.view.init($('body'));
+		});*/
+		obo.model = obo.model(obo.view, {});
+		console.log(obo.model);
+		obo.model.load(params, function() {
+			obo.view.init($('body'));
+		});
+		/*obo.model.loadInstance('1937', function() {
+			obo.view.init($('body'));
+		});*/
+		
+		
+	});
+	
+	
+	
+	//alert('you tube and obo.video.js');
 </script>
 </head>
 <body <?php body_class(); ?>>
