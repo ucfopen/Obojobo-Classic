8120038753adbf04a2a2f565ddcafa0995fb03a9
Ian Turgeon <iturgeon@gmail.com>
2011-12-08 20:40:14 -0500

    fixing rebase conficts

---
+++ wp/wp-content/themes/obojobo/page-embed-flex.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Embed Flex
+*/
+
+get_header('embed-flex'); ?>
+	<div id="flexApp">
+<div style="margin: 0 auto; margin-top: 4em; border: thin solid gray; padding: 20px; width: 500px; color: #222222; font-family: Verdana,sans-serif; font-size: 73%; line-height: 130%;">
+            <a style="padding-right: 20px; width: 158px; float: left; border: 0px;" href="http://www.adobe.com/go/getflashplayer"><img src="/assets/images/get_adobe_flash_player.png" alt="Download Flash Player" /></a>
+            <p style="padding: 0; margin: 0; float: left; width: 320px;">
+                Obojobo requires that you have the Flash Player plug-in (version 10 or greater) installed.<br /><a href="http://www.adobe.com/go/getflashplayer">Click here to download the latest version.</a>
+            </p>
+            <div style="clear:both;"></div>
+        </div>
+</div>
+<?php get_footer('embed-flex'); ?>
