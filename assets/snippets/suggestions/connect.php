974f654f9d365367f289cf476579b9b329c25a2c
Ian Turgeon <iturgeon@gmail.com>
2011-12-08 20:40:14 -0500

    fixing rebase conficts

--- assets/snippets/suggestions/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once(dirname(__FILE__) . '/../../../internal/app.php');
-
-$DBM = \rocketD\db\DBManager::getConnection(new \rocketD\db\DBConnectData(\AppCfg::DB_MODX_HOST, \AppCfg::DB_MODX_USER, \AppCfg::DB_MODX_PASS, \AppCfg::DB_MODX_NAME, \AppCfg::DB_MODX_TYPE));
-?>
