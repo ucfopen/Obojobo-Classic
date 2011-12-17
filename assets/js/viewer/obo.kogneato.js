b3aac690e46001ae2f7c1c5b8198212b9c60b7fc
Zachary Berry <zachberry@gmail.com>
2011-12-16 23:55:47 -0500

    added reload media button for quiz captivates, added no flash message in assessment, removed hr for media only content pages

---
+++ assets/js/viewer/obo.kogneato.js
@@ -0,0 +1,91 @@
+// Handles scoring from captivate spy swfs and 
+
+if(!window.obo)
+{
+	window.obo = {};
+}
+
+obo.captivate = function()
+{
+	// Map the GIID to the kogneato URL
+	var kogneatoURLs = {};
+	
+	// public method which ExternalInterface calls whenever it gets a captivate event.
+	// look in the CaptivateSpy fla files for the ExternalInterface code
+	var onCaptivateSpyEvent = function(event)
+	{
+		debug.log('onCaptivateSpyEvent',event, event.type, event.id,scoreDatas);
+		
+		/*
+		// if this event is for a different id then reset our data
+		if(event.id != captivateID)
+		{
+			captivateID = event.id;
+			scoreData = {responses:[], numQuestionsAnswered:0};
+		}*/
+		if(scoreDatas[event.id] == undefined)
+		{
+			scoreDatas[event.id] = {responses:[], numQuestionsAnswered:0};
+		}
+		
+		var scoreData = scoreDatas[event.id];
+		
+		// we listen for two versions of captivate - 2 (AS2) and 5 (AS3):
+		switch(event.version)
+		{
+			case 2:
+				switch(event.type)
+				{
+					case 'score':
+						obo.view.updateInteractiveScore(Math.round(event.data.percent * 100));
+						break;
+				}
+				break;
+			case 5:
+			default: // assume newer version of captivate if we don't know
+				switch(event.type)
+				{
+					case 'CPInteractiveItemSubmitEvent':
+					case 'CPQuestionSubmitEvent':
+						var page = obo.model.getPage();
+						
+						var questionEventData = event.data.questionEventData;
+						
+						var index = questionEventData.questionNumber;
+						//check to see if the question was already answered
+						if(scoreData.responses[index] == undefined)
+						{
+							scoreData.numQuestionsAnswered++;
+						}
+						
+						// store response
+						scoreData.responses[index] = (questionEventData.questionScore / questionEventData.questionMaxScore);
+						
+						// if all questions answered, send info to obojobo
+						var total = 0;
+						for(var i in scoreData.responses)
+						{
+							total += scoreData.responses[i];
+						}
+						
+						var percent = Math.round((total / event.data.cpQuizInfoTotalQuestionsPerProject) * 100);
+						
+						obo.view.updateInteractiveScore(percent);
+						break;
+				}
+				break;
+		}
+	};
+	
+	// removes all scoring information.
+	var clearCaptivateData = function()
+	{
+		scoreDatas = {};
+	};
+	
+	// @public:
+	return {
+		onCaptivateSpyEvent: onCaptivateSpyEvent,
+		clearCaptivateData: clearCaptivateData
+	}
+}();
