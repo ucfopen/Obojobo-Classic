b579804b9f5ed20d65e4e3ae6f806599efa2032b
Ian Turgeon <iturgeon@gmail.com>
2011-12-18 12:29:07 -0500

    Added preview mode to the viewer login screen

--- viewer.php
+++ viewer.php
@@ -4,7 +4,7 @@ require_once("internal/app.php");
 $API = \obo\API::getInstance();
 
 
-// If login crudentials sent attempt to log in
+// ================ LOGIN OR CHECK LOGIN ===========================
 if( isset($_REQUEST['username']) && isset($_REQUEST['password']) )
 {
   $loggedIn = $API->doLogin($_REQUEST['username'],  $_REQUEST['password']);
@@ -14,22 +14,45 @@ else
   $loggedIn = $API->getSessionValid();  
 }
 
-if($loggedIn === true)
+// ================ DISPLAY OUTPUT =================================
+if($loggedIn === true) // logged in, show the viewer
 {
+  
   include("assets/templates/viewer-main.php");
 }
-else
+else // not logged in
 {
-  $instData = $API->getInstanceData($_REQUEST['instID']);
-  
-  $title = $instData->name;
-  $course = $instData->courseID;
-  $instructor = $instData->userName;
-  $startDate = date('m/j/y', $instData->startTime);
-  $startTime = date('g:s a', $instData->startTime);
-  $endDate = date('m/j/y', $instData->endTime);
-  $endTime = date('g:s a', $instData->endTime);
 
+  // ================ PREPARE VARS FOR THE TEMPLATE ================
+
+
+  // Instance requested - student mode
+  if(isset($_REQUEST['instID']))
+  {
+    $instData = $API->getInstanceData($_REQUEST['instID']);
+    $title = $instData->name;
+    $course = $instData->courseID;
+    $instructor = $instData->userName;
+    $startDate = date('m/j/y', $instData->startTime);
+    $startTime = date('g:s a', $instData->startTime);
+    $endDate = date('m/j/y', $instData->endTime);
+    $endTime = date('g:s a', $instData->endTime);   
+  }
+
+  // lo requested - preview mode
+  elseif(isset($_REQUEST['loID']))
+  {
+    $loMeta = $API->getLOMeta($_REQUEST['loID']);
+    $title = $loMeta->title . ' ' . $loMeta->version . '.' . $loMeta->subVersion;
+    $course = 'PREVIEW ONLY';
+    $instructor = 'only visible to authors';
+    $startDate = 'Date Here';
+    $startTime = 'Time Here';
+    $endDate = 'Date Here';
+    $endTime = 'Time Here';    
+  }
+ 
+  // =============== RENDER THE TEMPLATE ========================
   include("assets/templates/login.php");
 }
 
