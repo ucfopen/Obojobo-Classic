1c96d3ff0e624fd2d1ec502e8395010bb100abc8
Ian Turgeon <iturgeon@gmail.com>
2011-12-18 13:05:18 -0500

    updated login screen style to new defaults, 404 on object not found

--- viewer.php
+++ viewer.php
@@ -17,7 +17,6 @@ else
 // ================ DISPLAY OUTPUT =================================
 if($loggedIn === true) // logged in, show the viewer
 {
-  
   include("assets/templates/viewer-main.php");
 }
 else // not logged in
@@ -29,30 +28,44 @@ else // not logged in
   // Instance requested - student mode
   if(isset($_REQUEST['instID']))
   {
-    $instData = $API->getInstanceData($_REQUEST['instID']);
-    $title = $instData->name;
-    $course = $instData->courseID;
-    $instructor = $instData->userName;
-    $startDate = date('m/j/y', $instData->startTime);
-    $startTime = date('g:s a', $instData->startTime);
-    $endDate = date('m/j/y', $instData->endTime);
-    $endTime = date('g:s a', $instData->endTime);   
+    if($instData = $API->getInstanceData($_REQUEST['instID']))
+    {
+      $title = $instData->name;
+      $course = $instData->courseID;
+      $instructor = $instData->userName;
+      $startDate = date('m/j/y', $instData->startTime);
+      $startTime = date('g:s a', $instData->startTime);
+      $endDate = date('m/j/y', $instData->endTime);
+      $endTime = date('g:s a', $instData->endTime);
+    }
+    else
+    {
+      header("HTTP/1.0 404 Not Found");
+      exit();
+    }
   }
 
   // lo requested - preview mode
   elseif(isset($_REQUEST['loID']))
   {
-    $loMeta = $API->getLOMeta($_REQUEST['loID']);
-    $title = $loMeta->title . ' ' . $loMeta->version . '.' . $loMeta->subVersion;
-    $course = 'PREVIEW ONLY';
-    $instructor = 'only visible to authors';
-    $startDate = 'Date Here';
-    $startTime = 'Time Here';
-    $endDate = 'Date Here';
-    $endTime = 'Time Here';    
+    if($loMeta = $API->getLOMeta($_REQUEST['loID']))
+    {
+      $title = $loMeta->title . ' ' . $loMeta->version . '.' . $loMeta->subVersion;
+      $course = 'PREVIEW ONLY';
+      $instructor = 'only visible to authors';
+      $startDate = 'Date Here';
+      $startTime = 'Time Here';
+      $endDate = 'Date Here';
+      $endTime = 'Time Here';    
+    }
+    else
+    {
+      header("HTTP/1.0 404 Not Found");
+      exit();
+    }
   }
  
-  // =============== RENDER THE TEMPLATE ========================
+  // =============== RENDER LOGIN TEMPLATE ========================
   include("assets/templates/login.php");
 }
 
