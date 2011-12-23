3f6963171e9ba26d287492c4ba09cd88adbb9037
Ian Turgeon <iturgeon@gmail.com>
2011-12-23 17:33:08 -0500

    fixed get courses admin script

--- internal/admin/WC_Get_Courses.php
+++ internal/admin/WC_Get_Courses.php
@@ -1,6 +1,7 @@
-<h2>List A User's Courses:</h2>
-<form action="<?php echo $_SERVER['PHP_SELF'] . '?' . $_SERVER["QUERY_STRING"]; ?> method="get" accept-charset="utf-8">
+<h2>List A Users Courses:</h2>
+<form action="<?php echo $_SERVER['PHP_SELF'] . '?' . $_SERVER['QUERY_STRING']; ?> method="get" accept-charset="utf-8">
 	<label for="nid">NID</label><input type="text" name="nid" value="" id="nid">
+	<?php echo rocketD_admin_tool_get_form_page_input(); ?>
 	<p><input type="submit" value="Continue &rarr;"></p>
 </form>
 <pre>
