c18872c2121b5a76cd7cffe4341acea7f342a9c3
Zachary Berry <zachberry@gmail.com>
2012-01-03 11:32:18 -0500

    fixed an issue where wrong section id was being sent via trackSectionChanged. updated Interaction_Log to work in wordpress

--- internal/admin/Interaction_Log.php
+++ internal/admin/Interaction_Log.php
@@ -1,33 +1,53 @@
-<pre>
 <?php
-
 $DBM = \rocketD\db\DBManager::getConnection(new \rocketD\db\DBConnectData(\AppCfg::DB_HOST, \AppCfg::DB_USER, \AppCfg::DB_PASS, \AppCfg::DB_NAME, \AppCfg::DB_TYPE));
 
 $TM = new \obo\log\LogManager();
+$AM = new \rocketD\auth\AuthManager();
 $time = microtime(1);
-if($_REQUEST['uid'])
+
+echo '<h1>Choose one:</h1><form action="'. $_SERVER['PHP_SELF'] .'" method="get" accept-charset="utf-8">
+	<ul>
+	<li><label for="nid">NID</label><input type="text" name="nid" value="" id="nid">
+	'.rocketD_admin_tool_get_form_page_input().'</li>
+	<li><label for="uid">UID</label><input type="text" name="uid" value="" id="uid">
+	'.rocketD_admin_tool_get_form_page_input().'</li>
+	<li><label for="inst_id">InstID</label><input type="text" name="inst_id" value="" id="inst_id">
+	'.rocketD_admin_tool_get_form_page_input().'</li>
+	</ul>
+	<p><input type="submit" value="Continue &rarr;"></p>
+</form>';
+
+echo '<pre>';
+
+if($_GET['nid'])
 {
-	if($_REQUEST['inst_id'])
+	$u = $AM->fetchUserByUserName($_GET['nid']);
+	$_GET['uid'] = $u->userID;
+}
+
+if($_GET['uid'])
+{
+	if($_GET['inst_id'])
 	{
-		echo "tracking INSTANCE: {$_REQUEST['inst_id']},  UID: {$_REQUEST['uid']}\n";
-		$track = $TM->getInteractionLogByUserAndInstance($_REQUEST['inst_id'], $_REQUEST['uid']);
+		echo "tracking INSTANCE: {$_GET['inst_id']},  UID: {$_GET['uid']}\n";
+		$track = $TM->getInteractionLogByUserAndInstance($_GET['inst_id'], $_GET['uid']);
 	}
 	else
 	{
-		echo "tracking UID: {$_REQUEST['uid']}\n";
-		$track = $TM->getInteractionLogByUser($_REQUEST['uid']);
+		echo "tracking UID: {$_GET['uid']}\n";
+		$track = $TM->getInteractionLogByUser($_GET['uid']);
 	}
 }
-elseif($_REQUEST['lo_id'])
+elseif($_GET['lo_id'])
 {
 
 	
 	
 }
-elseif($_REQUEST['inst_id'])
+elseif($_GET['inst_id'])
 {
-	echo "tracking INSTID: {$_REQUEST['inst_id']}\n";
-	$track = $TM->getInteractionLogByInstance($_REQUEST['inst_id']);
+	echo "tracking INSTID: {$_GET['inst_id']}\n";
+	$track = $TM->getInteractionLogByInstance($_GET['inst_id']);
 }
 else
 {
@@ -35,7 +55,7 @@ else
 	// $track = $TM->getInteractionLogTotals($_REQUEST['inst_id']);
 }
 
-if($_REQUEST['formatTime'] == true)
+if($_GET['formatTime'] == true)
 {
 	foreach($track['visitLog'] AS $key => $visitLog)
 	{
