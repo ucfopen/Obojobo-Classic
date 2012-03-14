5fddfebc9e496fc98337e3d551c410c4330a3e84
Ian Turgeon <iturgeon@gmail.com>
2012-03-14 14:53:10 -0400

    update to pagelet for Spring 2011

--- sso/portal/orientation-academic-integrity.php
+++ sso/portal/orientation-academic-integrity.php
@@ -139,8 +139,8 @@ try
 		{
 			?>
 			<h2>Are you an incoming student?</h2>
-			<p>If you are a New Undergraduate Student or a New Master's Program Student you are required to complete all the Academic Integrity Modules listed below in your group.</p>
-			<p>You need to score <strong><?php echo $minScore; ?>% or higher on all required modules before January 23, 2012</strong>. Otherwise you will receive a hold that prevents you from registering for classes.</p>
+			<p>If you are a New Undergraduate Student or a New Master's Program Student <strong>admitted in Spring 2012</strong> or later, you are required to complete all the Academic Integrity Modules listed below in your group.</p>
+			<p>You need to score <strong><?php echo $minScore; ?>% or higher on all required modules before July 15, 2012</strong>. Otherwise you will receive a hold that prevents you from registering for classes.</p>
 			
 			<h3>New Undergraduate Students</h3>
 				<ul>
