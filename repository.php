6b4cc470668ec06b2eec8f55f3bfa927dab712a1
Zachary Berry <zachberry@gmail.com>
2012-04-11 15:47:25 -0400

    this fixes issue #106

--- repository.php
+++ repository.php
@@ -18,7 +18,7 @@
   }
 </style>
 <script type="text/javascript">
-  var SWF_URL = "Repository_2_0_3.swf";
+  var SWF_URL = "Repository_2_0_4.swf";
 
   // START PREVENT CLOSE
   window.onbeforeunload = confirmExit;
