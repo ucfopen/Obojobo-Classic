737456399e014096f2a2173204ca4b2fa3cedf52
Zachary Berry <zachberry@gmail.com>
2012-05-25 13:25:26 -0400

    Increasing question limit to 20

--- repository.php
+++ repository.php
@@ -18,7 +18,7 @@
   }
 </style>
 <script type="text/javascript">
-  var SWF_URL = "Repository_2_0_5.swf";
+  var SWF_URL = "Repository_2_0_6.swf";
 
   // START PREVENT CLOSE
   window.onbeforeunload = confirmExit;
