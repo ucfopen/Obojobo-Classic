58be4ba8b821802fa91814ded194b0fbc2565919
Zachary Berry <zachberry@gmail.com>
2012-05-31 14:55:01 -0400

    derivatives can now be made from drafts. added option to make derivatives from your own objects.

--- repository.php
+++ repository.php
@@ -18,7 +18,7 @@
   }
 </style>
 <script type="text/javascript">
-  var SWF_URL = "Repository_2_0_6.swf";
+  var SWF_URL = "Repository_2_0_7.swf";
 
   // START PREVENT CLOSE
   window.onbeforeunload = confirmExit;
