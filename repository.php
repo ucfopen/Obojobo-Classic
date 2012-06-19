021638b7178d0c578b2fa5da7cd6ae50a101d6f2
Zachary Berry <zachberry@gmail.com>
2012-06-19 13:49:45 -0400

    New repository which uses the embedded youtube player instead of a custom chromeless player

--- repository.php
+++ repository.php
@@ -18,7 +18,7 @@
   }
 </style>
 <script type="text/javascript">
-  var SWF_URL = "Repository_2_0_7.swf";
+  var SWF_URL = "Repository_2_0_8.swf";
 
   // START PREVENT CLOSE
   window.onbeforeunload = confirmExit;
