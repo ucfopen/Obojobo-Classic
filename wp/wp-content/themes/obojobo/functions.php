4ce0734999d013dbd37f50fd8c4d2e189abc159c
Ian Turgeon <iturgeon@gmail.com>
2012-08-16 16:36:32 -0400

    add google analytics to everything

--- wp/wp-content/themes/obojobo/functions.php
+++ wp/wp-content/themes/obojobo/functions.php
@@ -473,3 +473,21 @@ function twentyten_posted_in() {
 	);
 }
 endif;
+
+add_action('wp_head', 'add_google_analytics');
+function add_google_analytics()
+{
+	if(defined('\AppCfg::GOOGLE_ANALYTICS_ID'))
+	{
+		echo "<script type='text/javascript'>
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', '".\AppCfg::GOOGLE_ANALYTICS_ID."']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();</script>";
+	}
+}
