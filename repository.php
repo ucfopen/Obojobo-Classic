c39fcbd29dd38a5324539c690a7d4bae0158f28d
Zachary Berry <zachberry@gmail.com>
2012-09-25 11:34:30 -0400

    new repo release build which displays scores by questions more efficently

--- repository.php
+++ repository.php
@@ -18,7 +18,7 @@
   }
 </style>
 <script type="text/javascript">
-  var SWF_URL = "Repository_2_0_9.swf";
+  var SWF_URL = "Repository_2_0_10.swf";
 
   // START PREVENT CLOSE
   window.onbeforeunload = confirmExit;
