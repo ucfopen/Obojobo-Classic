03aa8dc1ac864d9ad4c0833cb3bb0071288376dc
Zachary Berry <zachberry@gmail.com>
2012-12-20 16:40:51 -0500

    fixed GLOBAL bug - filter function in permissions list was commented out

--- repository.php
+++ repository.php
@@ -18,7 +18,7 @@
   }
 </style>
 <script type="text/javascript">
-  var SWF_URL = "Repository_2_0_10.swf";
+  var SWF_URL = "Repository_2_0_11.swf";
 
   // START PREVENT CLOSE
   window.onbeforeunload = confirmExit;
