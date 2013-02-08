fbd90e291ded5039d69643d92f25f7366484a177
Ian Turgeon <iturgeon@gmail.com>
2013-02-08 11:09:47 -0500

    removed score syncing from cron job

--- internal/includes/cron15minute.php
+++ internal/includes/cron15minute.php
@@ -10,13 +10,6 @@ This script is the main scheduled task runner
 require_once(dirname(__FILE__)."/../app.php");
 ini_set('error_log', AppCfg::DIR_BASE.AppCfg::DIR_LOGS.'php_cron_'. date('m_d_y', time()) .'.txt');
 
-//******************************** SYNC FAILURE QUEUE ****************************
-$t = microtime(1);
-$PM = \rocketD\plugin\PluginManager::getInstance();
-$result = $PM->callAPI('UCFCourses', 'sendFailedScoreSetRequests', array(), true);
-\rocketD\util\Log::profile('cron', "'sendFailedScoreSetRequests','".time()."','".round((microtime(true) - $t),5)."','{$result['updated']}','{$result['total']}'\n");
-
-
 //******************************** NID UPDATES ****************************
 $t = microtime(1);
 $AM = \rocketD\auth\AuthManager::getInstance();
