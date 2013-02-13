359887f162f767252a343afbaa2227328f1388e6
Zachary Berry <zachberry@gmail.com>
2013-02-13 14:16:59 -0500

    Added (R) symbol to Obojobo logos, removed unused throbber gifs for viewer

--- repository.php
+++ repository.php
@@ -18,7 +18,7 @@
   }
 </style>
 <script type="text/javascript">
-  var SWF_URL = "Repository_2_0_12.swf";
+  var SWF_URL = "Repository_2_0_13.swf";
 
   // START PREVENT CLOSE
   window.onbeforeunload = confirmExit;
