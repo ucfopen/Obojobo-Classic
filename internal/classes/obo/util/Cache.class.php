ea6368794ca6d28be95d423f442d650bd6dd19cb
Ian Turgeon <iturgeon@gmail.com>
2014-01-15 17:58:19 -0500

    remove unused logging from amfphp

--- internal/classes/obo/util/Cache.class.php
+++ internal/classes/obo/util/Cache.class.php
@@ -28,8 +28,6 @@ class Cache extends \rocketD\util\RDMemcache
 		
 		$this->connectMemCache();
 		// dump any profiler data to file
-		\rocketD\util\Log::dumpProfile('amfphp_Filters');
-		\rocketD\util\Log::dumpProfile('amfphp_Methods');
 		\rocketD\util\Log::dumpProfile('memcache_missed');
 		$this->flush();
 		
