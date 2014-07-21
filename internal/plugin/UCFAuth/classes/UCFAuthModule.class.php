3d96fc8dde92099b644f69f6c362f76108c96de1
Jonathan Warner <jaxbot@gmail.com>
2014-07-21 12:48:48 -0400

    Rip out old UCF code

--- internal/plugin/UCFAuth/classes/UCFAuthModule.class.php
+++ internal/plugin/UCFAuth/classes/UCFAuthModule.class.php
@@ -210,14 +210,8 @@ class plg_UCFAuth_UCFAuthModule extends \rocketD\auth\AuthModule
 
 		header("Location: " . $_SESSION['redirect']);
 
-		// filter the username to be all lowercase
-		$requestVars['userName'] = strtolower($requestVars['userName']);
-		
-		if($this->validateUsername($requestVars['userName']) !== true) return false;
-		if($validSSO == false && $this->validatePassword($requestVars['password']) !== true) return false;
-		
 		// create/update the user with the external database
-		$user = $this->syncSamlUser($requestVars['userName'], $weakExternalSync);
+		$user = $this->syncSamlUser($attributes[$attr[\cfg_plugin_AuthModUCF::USERNAME]], $weakExternalSync);
 		
 		if($user instanceof \rocketD\auth\User)
 		{
@@ -244,67 +238,6 @@ class plg_UCFAuth_UCFAuthModule extends \rocketD\auth\AuthModule
 			$validSSO = true;
 			\rocketD\util\Log::profile('login', "'".$requestVars['userName']."','LTI','0','".time().",'1'");
 		}
-	}
-
-	protected function checkForValidPortalSession(&$requestVars, &$validSSO, &$weakExternalSync)
-	{
-		if(isset($_SESSION['PORTAL_SSO_NID']) && isset($_SESSION['PORTAL_SSO_EPOCH']) && $_SESSION['PORTAL_SSO_EPOCH'] >= time() - 1800)
-		{
-			$requestVars['userName'] = $_SESSION['PORTAL_SSO_NID'];
-			// logged in, clear the session variables
-			unset( $_SESSION['PORTAL_SSO_NID'],  $_SESSION['PORTAL_SSO_EPOCH'] );
-			$validSSO = true;
-			$weakExternalSync = true; // allow the user to not exist in external db
-			\rocketD\util\Log::profile('login', "'".$requestVars['userName']."','PortalSSO','0','".time().",'1'");
-		}
-	}
-
-	public function verifyPassword($userName, $password)
-	{
-		
-		
-		// for local testing, ldap access may not be possible, if in local test mode just return an ok
-		if(\AppCfg::UCF_AUTH_BYPASS_PASSWORDS && $_SERVER['SERVER_ADDR'] != \AppCfg::PRODUCTION_IP)
-		{
-			trace('WARNING LOCAL AUTHENTICATION TEST MODE ENABLED', true);
-			return array('success' => true, 'code' => '');
-		}
-		
-		// make the LDAP request
-		$success = false;
-		$code = '';
-		$time = microtime(true); // timer for LDAP call
-		try
-		{
-			$ds = @ldap_connect(\AppCfg::LDAP);
-			if (!$ds)
-			{
-				$NM = \obo\util\NotificationManager::getInstance();
-				$NM->sendCriticalError('LDAP Connection Failure', 'Failed to connect to LDAP on ' . date("F j, Y, g:i a"));
-				trace('connecting to ldap failed', true);
-			}
-			else
-			{
-				ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
-				$success = @ldap_bind($ds, "cn=".$userName.",ou=people,dc=net,dc=ucf,dc=edu", $password); // true if LDAP verifies
-				\rocketD\util\Log::profile('login', "'$userName','LDAP','".round((microtime(true) - $time),5)."','".time().",'".($success?'1':'0')."'");
-			}
-		}
-		catch(Exception $e)
-		{
-			$NM = \obo\util\NotificationManager::getInstance();
-			$NM->sendCriticalError('Oracle DB Connection Failure', 'LDAP Threw an Error ' . date("F j, Y, g:i a") . "\r\n" . print_r($e, true));
-			trace('ldap threw and exception', true);
-			trace($e);
-		}
-		
-		return array('success' => $success, 'code' => $code);
-	}
-
-	// security check: Ian Turgeon 2008-05-08 - PASS
-	protected function createSalt()
-	{
-		return md5(uniqid(rand(), true));
 	}
 
 	// security check: Ian Turgeon 2008-05-08 - PASS
@@ -350,51 +283,6 @@ class plg_UCFAuth_UCFAuthModule extends \rocketD\auth\AuthModule
 	
 	}
 	
-	// security check: Ian Turgeon 2008-05-06 - PASS
-	public function validateUsername($userName)
-	{
-		// make sure the string length is less then 255, our usernames aren't that long
-		if(strlen($userName) > \cfg_plugin_AuthModUCF::MAX_USERNAME_LENGTH)
-		{
-			trace('User name maximum length is '.\cfg_plugin_AuthModUCF::MAX_USERNAME_LENGTH.' characters. ' . $userName, true);
-			return 'User name maximum length is 20 characters.';
-		}
-		// make sure the username is atleast 2 characters
-		if(strlen($userName) < \cfg_plugin_AuthModUCF::MIN_USERNAME_LENGTH)
-		{
-			trace('User name minimum length is '.\cfg_plugin_AuthModUCF::MIN_USERNAME_LENGTH.' characters. ' . $userName, true);
-			return 'User name minimum length is 2 characters.';
-		}			
-		if(empty($userName))
-		{
-			trace('Username is empty', true);
-			return 'Username is empty';
-		}
-		//if(preg_match("/^[[:alnum:]]{".\cfg_plugin_AuthModUCF::MIN_USERNAME_LENGTH.",".\cfg_plugin_AuthModUCF::MAX_USERNAME_LENGTH."}$/i", $userName) == false)
-		if(preg_match("/^[[:alnum:]_]{".\cfg_plugin_AuthModUCF::MIN_USERNAME_LENGTH.",".\cfg_plugin_AuthModUCF::MAX_USERNAME_LENGTH."}$/i", $userName) == false)
-		{
-			trace('User name can only contain alpha numeric characters. ' . $userName, true);
-			return 'User name can only contain alpha numeric characters.';
-		}		
-		return true;
-	}
-
-	// security check: Ian Turgeon 2008-05-06 - PASS
-	public function validatePassword($pass)
-	{
-		if(empty($pass))
-		{
-			trace('password is empty');
-			return 'Password is an empty string';
-		}
-		if(\obo\util\Validator::isMD5($pass) && $pass == 'd41d8cd98f00b204e9800998ecf8427e')
-		{
-			trace('md5 password is empty');
-			return 'Password is an empty string';
-		}
-		return true;
-	}
-
 	public function syncSamlUser($userName, $attributes)
 	{
 		$email = "notreal@not.real";
@@ -421,7 +309,7 @@ class plg_UCFAuth_UCFAuthModule extends \rocketD\auth\AuthModule
 			{
 				// update user data changes
 				if($first != $user->first || $last != $user->last || $email != $user->email){
-					//trace('updating user info: ' . $user->mi .'='. $externalUser->{\cfg_plugin_AuthModUCF::MIDDLE} .','. $user->first .'='. $externalUser->{\cfg_plugin_AuthModUCF::FIRST}.','.$user->last .'='. $externalUser->{\cfg_plugin_AuthModUCF::LAST}.','.$user->email .'='. $externalUser->{\cfg_plugin_AuthModUCF::EMAIL});
+					trace('updating user info: ' . $user->mi .'=,'. $user->first .'='. $first.','.$user->last .'='. $last.','.$user->email .'='. $email);
 					// external record differs from ours, update ours to match the external data
 					$user->first = $first;
 					$user->last = $last;
@@ -461,104 +349,8 @@ class plg_UCFAuth_UCFAuthModule extends \rocketD\auth\AuthModule
 		return $user;
 	}
 	
-	public function syncExternalUser($userName, $allowWeakSync=false, $createIfMissing = false)
+	protected function trimArray($array)
 	{
-		if($this->validateUsername($userName) !== true) return false;
-
-		// look for user in external data
-		if($externalUser = $this->getUCFUserData($userName))
-		{
-		
-		// user didn't exist externally, however weakSync assumes SSO as authoritative, create a placeholder user account
-		if($allowWeakSync === true)
-		{
-			// if the placeholder already exists
-			if($userID = $this->getUIDforUsername($userName))
-			{
-				// load user data from db
-				if($user = $this->fetchUserByID($userID) )
-				{
-					return $user;
-				}
-				else
-				{
-					return false;
-				}
-			}
-			// no placeholder, create
-			else
-			{
-				// create internal placeholder record
-				$created = $this->createNewUser($userName, '', '', '', '', array());
-				if(!$created['success'])
-				{
-					trace('createNewUser Failed', true);
-					return false;
-				}
-				else
-				{
-					trace('weak Sync used for '. $userName, true);
-					// load user data from db
-					if( !( $user = $this->fetchUserByID( $this->getUIDforUsername($userName) ) ) )
-					{
-						trace('fetchUserByID failed', true);
-						return false;
-					}
-				
-					$qstr = "INSERT INTO obo_user_meta SET userID = '?', meta='?', value = '?'";
-					$result = $this->DBM->querySafe($qstr, $user->userID, 'portal_sso', '1'); // add record to track where the user was originally created
-					return $user;
-				}
-			}
-		}
-		return false;
-	}
-	
-	// TODO: FIX RETURN FOR DB ABSTRACTION
-	protected function getUCFUserData($userName)
-	{
-		//check memcache
-	
-		$return = false;
-		
-		// TODO: need to filter sql variables to prevent sql injection
-		$this->defaultDBM();
-		if(!$this->oDBM->connected || !$this->DBM->connected)
-		{
-			trace('not connected', true);
-			return false;
-		}
-		$this->updateNIDChanges(); //  update internal nids if needed
-		
-		// try faculty first
-		$q = $this->oDBM->querySafe("Select * FROM ".\cfg_plugin_AuthModUCF::TABLE_EMPLOYEE." WHERE ".\cfg_plugin_AuthModUCF::NID." = '?'", $userName);
-		if($r = $this->oDBM->fetch_obj($q))
-		{
-			$r->isCreator = true;
-			$return = $this->trimArray($r);
-		}
-		else
-		{
-			// try students second
-			$q = $this->oDBM->querySafe("Select * FROM ".\cfg_plugin_AuthModUCF::TABLE_STUDENT." WHERE ".\cfg_plugin_AuthModUCF::NID." = '?'", $userName);
-			if($r = $this->oDBM->fetch_obj($q))
-			{
-				$r->isCreator = false;
-				$return =  $this->trimArray($r);
-			}
-		}
-		
-		if($return)
-		{
-			
-			//store in memcache
-			\rocketD\util\Cache::getInstance()->setModUCFExternalUser($userName, $return);
-		}
-		trace($return ? "$userName found, employee:$r->isCreator" : "$userName not found");
-		return $return;
-	}
-	
-	protected function trimArray($array){
 		if(count($array) > 0 )
 		{
 			foreach($array as $value)
@@ -650,73 +442,6 @@ class plg_UCFAuth_UCFAuthModule extends \rocketD\auth\AuthModule
 		return false;
 	}
 
-	public function updateNIDChanges($force=false)
-	{
-		$total = 0;
-		$updated = 0;
-		// TODO: remove memcache for any updated users
-		$this->defaultDBM();
-		if(!$this->oDBM->connected || !$this->DBM->connected)
-		{
-			trace('not connected', true);
-			return false;
-		}
-		$lastUpdate = -1;
-		// get last successful update date
-		$q = $this->DBM->query("SELECT ".\cfg_core_Temp::VALUE." FROM ".\cfg_core_Temp::TABLE." WHERE ".\cfg_core_Temp::ID."='".\cfg_plugin_AuthModUCF::COL_EXTERNAL_SYNC_NAME."' ");
-		if($r = $this->DBM->fetch_obj($q))
-		{
-			// if the last time we checked was anytime after the first second of today, skip updates unless overrided
-			$now = getdate();
-			if($r->{\cfg_core_Temp::VALUE} > mktime(0,0, 0, $now['mon'], $now['mday'], $now['year']) && $force == false)
-			{
-				return array('updated' => $updated, 'total' => $total);
-			}
-			// convert to string for comparison with oracle
-			$lastUpdate = strftime("%d-%b-%y", $r->{\cfg_core_Temp::VALUE});
-			trace('looking for updates after '. $lastUpdate, true);
-		}
-		// get all updates since last update
-		if($q = $this->oDBM->query("SELECT * FROM ".\cfg_plugin_AuthModUCF::TABLE_NID." WHERE ".\cfg_plugin_AuthModUCF::NID_CHANGE_DATE." >= '$lastUpdate'")) // no need for querySafe
-		{ 
-
-			while($r = $this->oDBM->fetch_obj($q))
-			{
-				$total++;
-				// the latest update date will be first, lets keep track of it in case the EFFDT doesn't match up with every day
-				// update each NID
-				if($q2 = $this->DBM->querySafe("UPDATE ".\cfg_core_User::TABLE." SET ".\cfg_core_User::LOGIN."='?' WHERE ".\cfg_core_User::LOGIN."='?' LIMIT 1", $r->{\cfg_plugin_AuthModUCF::NEW_NID}, $r->{\cfg_plugin_AuthModUCF::OLD_NID}))
-				{
-					if($this->DBM->affected_rows($q2) != 0)
-					{
-						$updated++;
-						\rocketD\util\Cache::getInstance()->clearUserByID($userID);
-						trace('NID changed: ' . $r->{\cfg_plugin_AuthModUCF::OLD_NID} .'->'. $r->{\cfg_plugin_AuthModUCF::NEW_NID}, true);
-					}  
-					else
-					{
-						trace('NID change may not be needed: ' . $r->{\cfg_plugin_AuthModUCF::OLD_NID} .'->'. $r->{\cfg_plugin_AuthModUCF::NEW_NID}, true);
-					}
-				}
-				// double check to make sure the old NID isnt in our db anymore
-				$saftyQ = $this->DBM->querySafe("SELECT * FROM ".\cfg_core_User::TABLE." WHERE ".\cfg_core_User::LOGIN."='?'", $r->{\cfg_plugin_AuthModUCF::OLD_NID});
-				if($this->DBM->fetch_num($saftyQ) > 0 )
-				{
-					trace('NID change failed, record for old NID still exists in Obojobo: '. $r->{\cfg_plugin_AuthModUCF::OLD_NID} .'->'. $r->{\cfg_plugin_AuthModUCF::NEW_NID}, true);
-				}
-			}
-		}
-		// update last log
-		if($lastUpdate == -1)
-		{
-			$q = $this->DBM->query("INSERT INTO ".\cfg_core_Temp::TABLE." SET ".\cfg_core_Temp::ID."='".\cfg_plugin_AuthModUCF::COL_EXTERNAL_SYNC_NAME."', ".\cfg_core_Temp::VALUE."='". time() ."'");
-		}
-		else
-		{
-			$q = $this->DBM->query("UPDATE ".\cfg_core_Temp::TABLE." SET ".\cfg_core_Temp::VALUE."='". time() ."' WHERE ".\cfg_core_Temp::ID."='".\cfg_plugin_AuthModUCF::COL_EXTERNAL_SYNC_NAME."' ");
-		}
-		return array('updated' => $updated, 'total' => $total);
-	}
 	public function requestPasswordReset($userName, $email, $returnURL)
 	{
 		return false;
@@ -730,6 +455,11 @@ class plg_UCFAuth_UCFAuthModule extends \rocketD\auth\AuthModule
 	public function changePasswordWithKey($userName, $key, $newpass)
 	{
 		return false;
-	}	
+	}
+
+	public function verifyPassword($userName, $password)
+	{
+		return false;
+	}
 }
 ?>
