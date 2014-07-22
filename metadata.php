4e710d5496eaea7dbb6b32dedf30d80797b92b13
Jonathan Warner <jaxbot@gmail.com>
2014-07-22 11:48:45 -0400

    Nicer SAML config

--- metadata.php
+++ metadata.php
@@ -2,7 +2,7 @@
 require_once('internal/app.php');
 require('internal/plugin/UCFAuth/packages/php-saml/_toolkit_loader.php');
 
-$settings = new OneLogin_Saml2_Settings(\AppCfg::$SAML_SETTINGS['saml']['config']);
+$settings = new OneLogin_Saml2_Settings(\cfg_plugin_AuthModUCF::$SAML_CONFIG);
 $metadata = $settings->getSPMetadata();
 $errors = $settings->validateMetadata($metadata);
 
