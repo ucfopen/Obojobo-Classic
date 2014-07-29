852ef70a9f03fc458d69a7e1ac5b90ea5a357d87
Jonathan Warner <JO476543@tux.cdl.ucf.edu>
2014-07-29 12:10:36 -0400

    Working login system

--- viewer.php
+++ viewer.php
@@ -2,31 +2,7 @@
 require_once("internal/app.php");
 $API = \obo\API::getInstance();
 
-// ================ LOGIN OR CHECK EXISTING LOGIN ===========================
-if(( isset($_REQUEST['username']) && isset($_REQUEST['password'])) || isset($_REQUEST['SAMLResponse']))
-{
-  $url = $_SERVER['REQUEST_URI'];
-  if ($url != "/saml/acs")
-    setcookie("redir", $url, 0, "/");
-  else
-    $url = $_COOKIE['redir'];
-  if (!isset($_REQUEST['username']))
-    $_REQUEST['username'] = "";
-  if (!isset($_REQUEST['password']))
-    $_REQUEST['password'] = "";
-
-  $loggedIn = $API->doLogin($_REQUEST['username'],  $_REQUEST['password']);
-  if($loggedIn !== true)
-  {
-    $notice = 'Invalid Login';
-  } else {
-    header("Location: $url");
-  }
-}
-else
-{
-  $loggedIn = $API->getSessionValid();  
-}
+require("login.php");
 
 // ================= CHECK FOR REQUIRED ROLE =======================
 
