130e1c6ce905e2d9736ed0aff7227db6afc44da9
Jonathan Warner <JO476543@tux.cdl.ucf.edu>
2014-08-04 15:30:46 -0400

    Silently accept non-standard metadata

--- metadata.php
+++ metadata.php
@@ -4,16 +4,7 @@ require('internal/plugin/UCFAuth/packages/php-saml/_toolkit_loader.php');
 
 $settings = new OneLogin_Saml2_Settings(\cfg_plugin_AuthModUCF::$SAML_CONFIG);
 $metadata = $settings->getSPMetadata();
-$errors = $settings->validateMetadata($metadata);
 
-if (empty($errors))
-{
-  header('Content-Type: text/xml');
-  echo $metadata;
-} else {
-  throw new OneLogin_Saml2_Error(
-    'Invalid SP metadata: '.implode(', ', $errors),
-    OneLogin_Saml2_Error::METADATA_SP_INVALID
-  );
-}
+header('Content-Type: text/xml');
+echo $metadata;
 
