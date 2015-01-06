e6320ccfb9a5330fd73ef6c51b090b78f1b45e51
Ian Turgeon <ian@ucf.edu>
2015-01-06 13:17:33 -0500

    upgrade wordpress to 4.1

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -55,6 +55,9 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+/** ALLOW WP TO UPDATE ITSELF  **/
+define('FS_METHOD', 'direct');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
