e1a9ea81884a61bf6c53a7c23547af865bb27d46
Ian <iturgeon@gmail.com>
2015-10-02 18:01:05 -0400

    updates ucfauth lock to 0.0.9 and fixes path in metadata

--- saml_metadata.php
+++ saml_metadata.php
@@ -2,7 +2,7 @@
 require_once('internal/app.php');
 require(\AppCfg::DIR_BASE.'internal/vendor/ucfcdl/php-saml/_toolkit_loader.php');
 
-$samlConfig = include(\AppCfg::DIR_BASE.'includes/config/vendor/cfgSaml.php');
+$samlConfig = include(\AppCfg::DIR_BASE.'internal/config/vendor/cfgSaml.php');
 $settings = new OneLogin_Saml2_Settings($samlConfig);
 
 header('Content-Type: text/xml');
