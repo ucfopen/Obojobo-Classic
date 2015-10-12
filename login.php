476f1dce08e41361fbcfbe7d91941eb632872904
iturgeon <iturgeon@gmail.com>
2015-10-12 12:22:13 -0400

    moves saml specific login code into saml auth module

--- login.php
+++ login.php
@@ -1,38 +1,20 @@
 <?php
-// TODO: see if we can move this stuff into the SAML auth mod
 
-// ================ LOGIN OR CHECK EXISTING LOGIN ===========================
-//  cmdweblogin = login submit form, SAMLResponse = we just came back from SAML
-if (isset($_REQUEST['cmdweblogin']) || isset($_REQUEST['SAMLResponse']))
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
-	if ( ! isset($_REQUEST['username'])) $_REQUEST['username'] = "";
-	if ( ! isset($_REQUEST['password'])) $_REQUEST['password'] = "";
-	$url = $_SERVER['REQUEST_URI'];
-
-	// is this the saml Assertion Consumer Service?
-	if ($url != "/saml/acs")
-	{
-		setcookie("redir", $url, 0, "/"); //store the redirect location in a cookie
-	}
-	else
-	{
-		$url = $_COOKIE['redir']; // get the redirect location from the cookie
-		setcookie("redir", '', time() - 3600, "/"); // delete the cookie
-	}
-
-	// waterfall through the authentication modules
-	$loggedIn = $API->doLogin($_REQUEST['username'],  $_REQUEST['password']);
-
-	if ($loggedIn === true)
-	{
-		// Redirect
-		header("Location: $url");
-	}
-
-	// render page with this notice
-	$notice = 'Invalid Login';
+	$username = isset($_REQUEST['username']) ? $_REQUEST['username'] : '';
+	$password = isset($_REQUEST['password']) ? $_REQUEST['password'] : '';
+	$loggedIn = \rocketD\auth\AuthManager::getInstance()->login($username, $password);
 }
 else
 {
 	$loggedIn = $API->getSessionValid();
 }
+
+if ($loggedIn === true)
+{
+	header("Location: $_SERVER['REQUEST_URI']"); // Redirect
+}
+
+// render page with this notice
+$notice = 'Invalid Login';
