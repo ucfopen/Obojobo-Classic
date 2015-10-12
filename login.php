c9a4846a98ecbd6dbe946b9fad4a99d8a730b2f9
Ian Turgeon <iturgeon@gmail.com>
2015-10-12 15:46:25 -0400

    singleton trait now works for class inheritance, saml login cleaned up

--- login.php
+++ login.php
@@ -5,16 +5,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 	$username = isset($_REQUEST['username']) ? $_REQUEST['username'] : '';
 	$password = isset($_REQUEST['password']) ? $_REQUEST['password'] : '';
 	$loggedIn = \rocketD\auth\AuthManager::getInstance()->login($username, $password);
+	$notice = 'Invalid Login';
 }
 else
 {
 	$loggedIn = $API->getSessionValid();
 }
 
-if ($loggedIn === true)
-{
-	header("Location: $_SERVER['REQUEST_URI']"); // Redirect
-}
-
-// render page with this notice
-$notice = 'Invalid Login';
