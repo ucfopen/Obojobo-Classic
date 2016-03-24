b5ee694b409abd0e05ca74e226b4bb1e4d83ccc7
iturgeon <iturgeon@gmail.com>
2016-03-24 17:08:42 -0400

    fix bad reference to substr

--- lti/lti-test-return.php
+++ lti/lti-test-return.php
@@ -10,7 +10,7 @@ if ( ! function_exists('apache_request_headers'))
 	$headers = '';
 	foreach ($_SERVER as $name => $value)
 	{
-		if (subst($name, 0, 5) == 'HTTP_')
+		if (substr($name, 0, 5) == 'HTTP_')
 		{
 			$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
 		}
