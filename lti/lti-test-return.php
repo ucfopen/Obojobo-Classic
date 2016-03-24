08986b9261313b38d33ba45e91fdb3175c356a57
Ian Turgeon <iturgeon@gmail.com>
2016-03-24 16:26:11 -0400

    adds support of lti oauth vars sent via Authorization headers

--- lti/lti-test-return.php
+++ lti/lti-test-return.php
@@ -4,8 +4,25 @@ This file can handle outbound lti score messages from the lti test interface.
  */
 require_once("../internal/app.php");
 
+# process headers if apache isn't around
+if ( ! function_exists('apache_request_headers'))
+{
+	$headers = '';
+	foreach ($_SERVER as $name => $value)
+	{
+		if (subst($name, 0, 5) == 'HTTP_')
+		{
+			$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+		}
+	}
+}
+else
+{
+	# just use apache's request headers
+	$headers = apache_request_headers();
+}
+
 // grab the oauth information:
-$headers     = apache_request_headers();
 $authHeaders = $headers['Authorization'];
 $authHeaders = str_replace('OAuth ', '', $authHeaders);
 $authHeaders = explode(',', $authHeaders);
