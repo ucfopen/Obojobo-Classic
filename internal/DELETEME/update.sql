6f6ef6a1efe57d31401b30c282344200fe2765e1
iturgeon <iturgeon@gmail.com>
2016-08-12 13:59:23 -0400

    more sharing cleanup, removing ucf specific stuff

--- internal/DELETEME/update.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `obo_badges` (
-  `loID` bigint(255) NOT NULL,
-  `badgeID` bigint(255) NOT NULL,
-  `minScore` int(3) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`loID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
