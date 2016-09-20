5c91fe711b852af57044990dace07be53cf4478b
iturgeon <iturgeon@gmail.com>
2016-09-20 10:15:39 -0400

    updates amfphp and moves it into composer, fixes language cache
    AMFPHP updated from our custom implementation to an official composer lib
    AMFPHP plugins added to make it emulate how our api used to work
    
    added a new json api endpoint just for the viewer

--- api/viewer.php
+++ api/viewer.php
@@ -1,22 +1,25 @@
 <?php
+// This api endpoint is the newer endpoint
+// urls look like /api/viewer.php?m=createInstanceVisit&p1=1&p2=cats
+// /api/viewer.php?m=<method>&p1=<param1>&p2=<param2>
+// The service is assumed by defining defaultServiceName below
+// The newer format is in /api/viewer.php
+// ObojoboAmfphpGet assumes the service is loRepostitory
+
 require_once(dirname(__FILE__)."/../internal/app.php");
 
 $config = new \Amfphp_Core_Config();
 $config->checkArgumentCount = false;
 $config->serviceFolders = [\AppCfg::DIR_BASE . 'internal/includes/amfphpServices/'];
 $config->pluginsFolders[] = \AppCfg::DIR_BASE . 'internal/includes/amfphpPlugins';
-$config->disabledPlugins = ['AmfphpLogger', 'AmfphpErrorHandler','AmfphpMonitor','AmfphpJson', 'AmfphpGet', 'ObojoboAmfphpGet', 'LegacyAmfphpGet', 'AmfphpDummy', 'AmfphpDiscovery', 'AmfphpAuthentication', 'AmfphpVoConverter'];
+$config->disabledPlugins = ['AmfphpLogger', 'AmfphpErrorHandler','AmfphpMonitor', 'AmfphpDummy', 'AmfphpGet', 'LegacyAmfphpGet', 'AmfphpDiscovery', 'AmfphpAuthentication', 'AmfphpVoConverter'];
 
-// $voFolders[] = array(dirname(__FILE__) . '/NamespaceVo/', 'NVo');
-// $config->pluginsConfig['AmfphpVoConverter'] = array('voFolders' => $voFolders);
-//set this to enforce vo conversion. If you do that, only sending UserVo1 shall work, not UserVo2
 $config->pluginsConfig = [
 	'ObojoboAmfphpVoConverter' => [
 		'voFolders' => [\AppCfg::DIR_BASE . \AppCfg::DIR_CLASSES],
 		'enforceConversion' => false
 	]
 ];
-
 
 $gateway = \Amfphp_Core_HttpRequestGatewayFactory::createGateway($config);
 $gateway->service();
--- internal/includes/amfphpServices/loRepository.php
+++ internal/includes/amfphpServices/loRepository.php
@@ -1,4 +1,3 @@
 <?php
 class loRepository extends \obo\API{
 }
-?>
