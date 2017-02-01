1830a3ad043715171c440f02a776e244e5610dde
Ian Turgeon <iturgeon@gmail.com>
2017-02-01 18:18:57 -0500

    fix invalid login error that showed repository flash login screen

--- internal/includes/login.php
+++ internal/includes/login.php
@@ -3,8 +3,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
 	$username = isset($_REQUEST['username']) ? $_REQUEST['username'] : '';
 	$password = isset($_REQUEST['password']) ? $_REQUEST['password'] : '';
-	$loggedIn = \rocketD\auth\AuthManager::getInstance()->login($username, $password);
-	$notice = 'Invalid Login';
+	$result   = \rocketD\auth\AuthManager::getInstance()->login($username, $password);
+	$loggedIn = ($result === true && !($result instanceof \obo\util\Error));
+	$notice = $loggedIn ? '' : 'Invalid Login';
 }
 else
 {
