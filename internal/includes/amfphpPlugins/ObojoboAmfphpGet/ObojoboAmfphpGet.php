ff09795b513d7c39ac2b9669c93006fe2df00300
Ian Turgeon <iturgeon@gmail.com>
2017-12-06 12:40:09 -0500

    Removes api/viewer.php and allows JSON POST requests
    * api/viewer.php completely removed
    * api/json.php now accepts both weird query strings and POST requests
    * amfphp filter for loRepository.method/arg1/arg2 consolidated into ObojoboAmfphpGet filter
    * javascript files updated to use api/json.php
    * ObojoboAmfphpVoConverter updated to protect result objects when they are expected (rather then always converting objects to an array)
    
    adfadsf

--- internal/includes/amfphpPlugins/ObojoboAmfphpGet/ObojoboAmfphpGet.php
+++ internal/includes/amfphpPlugins/ObojoboAmfphpGet/ObojoboAmfphpGet.php
@@ -8,6 +8,11 @@ class ObojoboAmfphpGet implements Amfphp_Core_Common_IDeserializer, Amfphp_Core_
 	const CONTENT_TYPE = 'text/amfphpget';
 
 	/**
+	* Flag used to determine if we're using the legacy query strings used in Obojobo
+	*/
+	protected $use_legacy_query_string = false;
+
+	/**
 	 * return error details.
 	 * @see Amfphp_Core_Config::CONFIG_RETURN_ERROR_DETAILS
 	 * @var boolean
@@ -36,7 +41,18 @@ class ObojoboAmfphpGet implements Amfphp_Core_Common_IDeserializer, Amfphp_Core_
 	 * @return this or null
 	 */
 	public function filterHandler($handler, $contentType){
-		return $this;
+		// only use this handler if the request uri contains the old style of api request: http://server.com/api/script.php/loRepository.API_METHOD/arg1/arg2
+		if(strpos($_SERVER['REQUEST_URI'], 'loRepository.') !== false){
+			$this->use_legacy_query_string = true;
+			return $this;
+		}
+
+		// Only enable this filter if this is a get request using newer format: http://server.com/api/script.php?m=API_METHOD&p1=arg1....
+		if ($_SERVER['REQUEST_METHOD'] == 'GET' && strpos($_SERVER['QUERY_STRING'], 'm=') !== false ){
+			trace('Using ObojoboAmfphpGet');
+			return $this;
+		}
+
 	}
 
 	/**
@@ -61,20 +77,49 @@ class ObojoboAmfphpGet implements Amfphp_Core_Common_IDeserializer, Amfphp_Core_
 	 * @return the service call response
 	 */
 	public function handleDeserializedRequest($deserializedRequest, Amfphp_Core_Common_ServiceRouter $serviceRouter){
-		$serviceName = 'loRepository'; // force this for Obojobo
+		if($this->use_legacy_query_string){
+			$query = str_replace("{$_SERVER['SCRIPT_NAME']}/", '', $_SERVER['REQUEST_URI']); // loRepository.createInstanceVisit/1
+			$splits = explode('/', $query);
+			$service_and_method = array_shift($splits);
+			$s_and_m_splits = explode('.', $service_and_method);
+			$serviceName = $s_and_m_splits[0];
+			$methodName = $s_and_m_splits[1];
+			$parameters = $splits;
+			// check for arrays that need to be parsed
+			// ie: .com/api/json.api/class.method/[1,2,3]/a
+			// this makes sure parameters[0] = array(1,2,3)
+			if(count($parameters))
+			{
+				foreach ($parameters as $key => $value)
+				{
+					if(substr($value, 0, 1) == '[' && substr($value, -1) == ']')
+					{
+						$parameters[$key] = explode(',', substr($value, 1, -1));
+					}
+				}
+			}
+			return $serviceRouter->executeServiceCall($serviceName, $methodName, $parameters);
+		}
+		else{
+			$serviceName = 'loRepository'; // force this for Obojobo
 
-		if(isset ($deserializedRequest['m'])){
-			$methodName = $deserializedRequest['m'];
-		}else{
-			throw new Exception('MethodName field missing in call parameters \n' . print_r($deserializedRequest, true));
+			trace('======================================');
+			trace($deserializedRequest);
+
+			if(isset ($deserializedRequest['m'])){
+				$methodName = $deserializedRequest['m'];
+			}else{
+				throw new Exception('MethodName field missing in call parameters \n' . print_r($deserializedRequest, true));
+			}
+			$parameters = array();
+			$paramCounter = 1;
+			while(isset ($deserializedRequest["p$paramCounter"])){
+				$parameters[] = $deserializedRequest["p$paramCounter"];
+				$paramCounter++;
+			}
+			return $serviceRouter->executeServiceCall($serviceName, $methodName, $parameters);
 		}
-		$parameters = array();
-		$paramCounter = 1;
-		while(isset ($deserializedRequest["p$paramCounter"])){
-			$parameters[] = $deserializedRequest["p$paramCounter"];
-			$paramCounter++;
-		}
-		return $serviceRouter->executeServiceCall($serviceName, $methodName, $parameters);
+
 
 	}
 
