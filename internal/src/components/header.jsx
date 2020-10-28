4ba8b6aa02e670ca15b564385452c8085f9570f4
Mauricio <mauriciofigueiredo@knights.ucf.edu>
2020-10-28 14:36:01 -0300

    Added the header component files, obo logo, and file loader

--- internal/src/components/header.jsx
+++ internal/src/components/header.jsx
@@ -1,8 +1,40 @@
+import './header.scss'
+
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default function Header() {
-	return <div>Header @TODO</div>
+import ObojoboLogo from '../../../assets/images/viewer/obojobo-logo.png'
+
+export default function Header(props) {
+	return (
+		<div className="obo-header">
+			<div className="obo-header--left-side">
+				<img className="obo-classic-logo" src={ObojoboLogo} />
+
+				<button className="header-btn" onClick={e => props.onClickAboutOrBannerLink(e)}>
+					About
+				</button>
+			</div>
+
+			<div className="banner-header">
+				<span>What&apos;s different?</span>
+				<button
+					className="banner-header--modal-button"
+					onClick={e => props.onClickAboutOrBannerLink(e)}
+				>
+					Click here to find out about the new look and our new version
+				</button>
+				<button className="banner-header--close-button">&#10005;</button>
+			</div>
+
+			<div className="obo-header--right-side">
+				<p>{props.userName}</p>
+				<button className="header-btn" onClick={e => props.onClickLogOut(e)}>
+					Logout
+				</button>
+			</div>
+		</div>
+	)
 }
 
 Header.defaultProps = {
--- internal/src/webpack.config.js
+++ internal/src/webpack.config.js
@@ -114,6 +114,10 @@ module.exports =
 								}
 							}
 						]
+					},
+					{
+						test: /\.(png|svg|jpg|gif)$/,
+						use: ['file-loader']
 					}
 				]
 			},
