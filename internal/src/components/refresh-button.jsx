bc99ba3e6e6b0513d77171adaceb8e4005f85393
ctcuff <dev.ctcuff@gmail.com>
2020-10-29 13:55:35 -0400

    Added refresh button component

--- internal/src/components/refresh-button.jsx
+++ internal/src/components/refresh-button.jsx
@@ -1,8 +1,9 @@
+import './refresh-button.scss'
 import React from 'react'
 import PropTypes from 'prop-types'
 
 export default function RefreshButton({ onClick }) {
-	return <button onClick={onClick}>Refresh Button @TODO</button>
+	return <button onClick={onClick} className="refresh-button" title="refresh"></button>
 }
 
 RefreshButton.propTypes = {
