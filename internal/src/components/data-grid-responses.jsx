8b65fe85a2f354370ded801a3b9882d096709149
Jacob Peterson <jpeterson976@gmail.com>
2020-11-02 15:41:15 -0500

    adds datagrid responses component

--- internal/src/components/data-grid-responses.jsx
+++ internal/src/components/data-grid-responses.jsx
@@ -1,8 +1,36 @@
+/* eslint-disable react/jsx-key */
+
 import React from 'react'
 import PropTypes from 'prop-types'
+import DataGridTimestampCell from './data-grid-timestamp-cell'
 
-export default function DataGridResponses() {
-	return <div>@TODO</div>
+import './data-grid.scss'
+
+export default function DataGridResponses({ responses }) {
+	return (
+		<table className="repository--data-grid">
+			<thead>
+				<tr>
+					<th>Student</th>
+					<th>Response</th>
+					<th>Score</th>
+					<th>Time</th>
+				</tr>
+			</thead>
+			<tbody>
+				{responses.map(response => (
+					<tr>
+						<td>{response.userName}</td>
+						<td>{response.response}</td>
+						<td>{response.score}</td>
+						<td>
+							<DataGridTimestampCell value={response.time} />
+						</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	)
 }
 
 DataGridResponses.propTypes = {
