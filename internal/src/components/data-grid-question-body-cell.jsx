efdcc5eaf459903d37a97890625d47831309a6e0
Zachary Berry <zachberry@gmail.com>
2020-11-03 13:35:56 -0500

    Adds DataGridQuestionBodyCell

--- internal/src/components/data-grid-question-body-cell.jsx
+++ internal/src/components/data-grid-question-body-cell.jsx
@@ -1,8 +1,65 @@
+import './data-grid-question-body-cell.scss'
+
 import React from 'react'
 import PropTypes from 'prop-types'
+import FlashHTML from './flash-html'
 
-export default function DataGridQuestionBodyCell() {
-	return <div>@TODO</div>
+const renderMediaItem = ({ title, itemType }) => {
+	switch (itemType) {
+		case 'pic':
+			return 'Image: ' + title
+
+		case 'swf':
+			return 'Flash: ' + title
+
+		case 'flv':
+			return 'Video: ' + title
+
+		case 'mp3':
+			return 'Audio: ' + title
+
+		case 'kogneato':
+			return 'Materia: ' + title
+	}
+}
+
+const renderItem = ({ component, data, media }) => {
+	switch (component) {
+		case 'TextArea':
+			return (
+				<div className="text-area">
+					<FlashHTML value={data} />
+				</div>
+			)
+
+		case 'MediaView':
+			return renderMediaItem(media[0])
+	}
+}
+
+export default function DataGridQuestionBodyCell({ items }) {
+	switch (items.length) {
+		case 1:
+			return (
+				<div className="repository--data-grid-question-body-cell is-not-multiple-items">
+					{renderItem(items[0])}
+				</div>
+			)
+
+		case 2:
+			return (
+				<div className="repository--data-grid-question-body-cell is-multiple-items">
+					<div className="row">
+						<span className="label">L:</span>
+						{renderItem(items[0])}
+					</div>
+					<div className="row">
+						<span className="label">R:</span>
+						{renderItem(items[1])}
+					</div>
+				</div>
+			)
+	}
 }
 
 DataGridQuestionBodyCell.propTypes = {
@@ -12,12 +69,8 @@ DataGridQuestionBodyCell.propTypes = {
 			data: PropTypes.string,
 			media: PropTypes.arrayOf(
 				PropTypes.shape({
-					mediaID: PropTypes.number,
 					title: PropTypes.string,
-					itemType: PropTypes.oneOf(['pic', 'kogneato', 'swf', 'flv', 'mp3']),
-					descText: PropTypes.string,
-					width: PropTypes.number,
-					height: PropTypes.number
+					itemType: PropTypes.oneOf(['pic', 'kogneato', 'swf', 'flv', 'mp3'])
 				})
 			)
 		})
