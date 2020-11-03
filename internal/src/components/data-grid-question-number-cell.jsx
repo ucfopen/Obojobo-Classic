e0a1886ca1ca14f70737879cbd00e7eb284826bd
Mauricio <mauriciofigueiredo@knights.ucf.edu>
2020-11-03 10:47:05 -0300

    Added the DataGridQuestionNumberCell

--- internal/src/components/data-grid-question-number-cell.jsx
+++ internal/src/components/data-grid-question-number-cell.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './data-grid-question-number-cell.scss'
 
-export default function DataGridQuestionNumberCell() {
-	return <div>@TODO</div>
+export default function DataGridQuestionNumberCell(props) {
+	const altInfo = props.altNumber ? <p>Alternate {props.altNumber}/2</p> : null
+
+	return (
+		<div className="data-grid-question-number-cell">
+			<p className="question-info">Question {props.displayNumber}:</p>
+			{altInfo}
+		</div>
+	)
 }
 
 DataGridQuestionNumberCell.defaultProps = {
