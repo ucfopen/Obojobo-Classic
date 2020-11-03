5367404db6b946a91e75d712add15bafd863a4e3
Mauricio <mauriciofigueiredo@knights.ucf.edu>
2020-11-03 12:13:46 -0300

    Created the ModalScoresByQuestion component

--- internal/src/components/modal-scores-by-question.jsx
+++ internal/src/components/modal-scores-by-question.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import DataGridScoresByQuestions from './data-grid-scores-by-question'
+import QuestionScoreDetails from './question-score-details'
+import './modal-scores-by-question.scss'
 
 export default function ModalScoresByQuestion() {
-	return <div>@TODO</div>
+	return (
+		<div className="modal-scores-by-question">
+			<div className="scores-by-question--left-sidebar">
+				<h2>Scores By Question</h2>
+				<DataGridScoresByQuestions />
+			</div>
+
+			<div className="score-details-right-content">
+				<QuestionScoreDetails />
+			</div>
+		</div>
+	)
 }
 
 ModalScoresByQuestion.propTypes = {
