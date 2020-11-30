386d0c159c23c36366f09b53fd3789daa136dcbf
iturgeon <iturgeon@gmail.com>
2020-11-30 10:19:52 -0500

    fix score details click after simplifying input

--- internal/src/components/data-grid-student-score-cell.jsx
+++ internal/src/components/data-grid-student-score-cell.jsx
@@ -4,8 +4,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Button from './button'
 
-export default function DataGridStudentScoreCell({ value, row, onClick }) {
-	const click = React.useCallback(() => {onClick(row.original.user, row.original.userID)}, [row, onClick])
+export default function DataGridStudentScoreCell({ value, row, column }) {
+	const { onClick } = column
+	const click = React.useCallback(() => {
+		onClick(row.original.user, row.original.userID)
+	}, [row, onClick])
 	return (
 		<div className="data-grid-student-score-cell">
 			<span className="data-grid-student-score-cell--score">{value ?? '--'}</span>
