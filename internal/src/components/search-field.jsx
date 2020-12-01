58d241c7ab56d9401069c43fa9c75f41096f688a
iturgeon <iturgeon@gmail.com>
2020-12-01 12:52:20 -0500

    add inputref to search

--- internal/src/components/search-field.jsx
+++ internal/src/components/search-field.jsx
@@ -3,18 +3,24 @@ import PropTypes from 'prop-types'
 
 import './search-field.scss'
 
-export default function SearchField({ value, onChange, placeholder }) {
-	const handleChange = React.useCallback(
-		e => {
-			if (onChange) onChange(e.target.value)
-		},
-		[onChange]
-	)
+export default function SearchField({ value, onChange, placeholder, focusOnMount}) {
+	const inputEl = React.useRef(null)
+
+	const handleChange = React.useCallback(e => {
+		if (onChange) onChange(e.target.value)
+	}, [onChange])
+
+	React.useEffect(() => {
+		if (focusOnMount === true) inputEl.current.focus()
+	}, [])
 
 	return (
-		<div className={'repository--search-field ' + (value ? 'is-not-empty' : 'is-empty')}>
+		<div
+			className={'repository--search-field ' + (value ? 'is-not-empty' : 'is-empty')}
+		>
 			<i className="magnifier-icon"></i>
 			<input
+				ref={inputEl}
 				className="search-field"
 				type="search"
 				name="search"
